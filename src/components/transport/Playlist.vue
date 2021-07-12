e91138bfc4a7cf870fb14d9e117107d4aa4cb199
James Gaither <jgaither091@gmail.com>
2021-07-12 13:53:49 -0400

    fix: tweak bottom spacing for the playlist on mobile
    fixed #4

--- src/components/transport/Playlist.vue
+++ src/components/transport/Playlist.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<button class="inline-block" @click="showPlaylist = !showPlaylist"><i class="text-xl fas fa-bars" /></button>
-		<div v-if="tracklist.length > 0 && showPlaylist" class="playlist absolute bg-white border bottom-28 h-96 md:right-8 md:left-auto right-0 left-0 rounded-md p-4 md:w-1/3 w-full shadow-md">
+		<div v-if="tracklist.length > 0 && showPlaylist" class="playlist absolute bg-white border md:bottom-28 bottom-24 h-96 md:right-8 md:left-auto right-0 left-0 rounded-md p-4 md:w-1/3 w-full shadow-md">
 			<div class="h-full w-full overflow-x-hidden overflow-auto rounded-md">
 				<div v-for="song in playlist" :key="song" class="p-2 flex flex-row gap-2" :class="{'bg-gray-100': song === playlist[trackIdx]}">
 					<div class="flex-none">
