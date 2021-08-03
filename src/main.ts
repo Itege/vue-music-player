ccb5e37ff740cb9b02ad2c175061d5fa66e4c05c
Itege <jgaither091@gmail.com>
2021-08-03 14:42:27 -0400

    fix: Do not reference volume.value when adjusting volume

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {TransportController} from '@/ApplicationState';
 import configureMediaSession from "@/ConfigureMediaSession";
 
 const setVolume = (offset: number): void => {
-	let volume: number = parseFloat(TransportController.volume.value.toFixed(1));
+	let volume: number = parseFloat(TransportController.volume.toFixed(1));
 	volume += offset;
 	volume = (volume < 0 ? 0 : (volume > 1 ? 1 : volume));
 	TransportController.setVolume(volume);
