c8167f959718f53f28daeb4501062d6d127bfbf4
Itege <jgaither091@gmail.com>
2021-08-03 14:13:47 -0400

    style: Move playlist even more to the right

--- src/components/transport/Playlist.vue
+++ src/components/transport/Playlist.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<button class="inline-block" @click="showPlaylist = !showPlaylist"><i class="text-xl fas fa-bars" /></button>
-		<div v-if="tracklist.length > 0 && showPlaylist" class="playlist absolute bg-white border bottom-24 h-1/2 lg:right-4 lg:left-auto right-0 left-0 rounded-md p-4 lg:w-1/3 w-full shadow-md flex flex-col">
+		<div v-if="tracklist.length > 0 && showPlaylist" class="playlist absolute bg-white border bottom-24 h-1/2 lg:right-2 lg:left-auto right-0 left-0 rounded-md p-4 lg:w-1/3 w-full shadow-md flex flex-col">
 			<div class="text-left text-xl pb-4 font-bold">Now Playing</div>
 			<div class="h-full w-full overflow-x-hidden overflow-auto rounded-md flex-grow">
 				<div v-for="(song, idx) in playlist" :key="song" class="p-2 flex flex-row gap-2 hover:bg-gray-200 cursor-pointer" :class="{'bg-gray-100': idx === trackIdx}" @dblclick="playSong(idx)">
